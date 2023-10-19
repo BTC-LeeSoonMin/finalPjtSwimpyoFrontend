9ee2f1ab422a667bb700647986c2a4709b25d3f4
congyubin52 <congyubin52@gamil.com>
2023-10-19 19:20:17 +0900

    dir structor

---
+++ src/pages/main/user/Main.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Container, Grid } from '@mui/material';
+
+function Main() {
+
+  return (
+    <Container component="main" maxWidth="xs" sx={{ marginBottom: '1rem', marginTop: '1rem' }}>
+      이미지 슬라이드
+    </Container>
+  );
+}
+
+export default Main;
