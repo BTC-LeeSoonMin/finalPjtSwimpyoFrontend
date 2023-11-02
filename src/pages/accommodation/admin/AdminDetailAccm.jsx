4f611261e09fa9fc6221bec6c1ad853708e84fc2
jinbeom <jinbeom3238@gmail.com>
2023-11-02 11:38:59 +0900

    modify rerendering

--- src/pages/accommodation/admin/AdminDetailAccm.jsx
+++ src/pages/accommodation/admin/AdminDetailAccm.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Backdrop, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, IconButton } from '@mui/material';
+import { Backdrop, CircularProgress, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, IconButton } from '@mui/material';
 import Carousel from 'react-material-ui-carousel'
 import { Paper, Button } from '@mui/material'
 import { styled } from '@mui/material/styles';
@@ -32,9 +32,14 @@ const AdminDetailAccm = () => {
         boxShadow: 'none'
     }));
 
+    const [dataLoaded, setDataLoaded] = useState(false);
 
-    const [accmData, setAccmData] = useState({}); // 백엔드에서 넘어온 데이터 넣는곳
-    const [images, setImages] = useState([]);
+    // const [accmData, setAccmData] = useState({}); // 백엔드에서 넘어온 데이터 넣는곳
+    // const [images, setImages] = useState([]);
+    const [backEndData, setBackEndData] = useState({
+        accmData: {},
+        accmImages: []
+    })
     const requestData = useParams(); // 넘어온 데이터
     const navigate = useNavigate();
 
@@ -50,7 +55,7 @@ const AdminDetailAccm = () => {
     const [isAddingRoom, setIsAddingRoom] = useState(false);
 
     // 등록에 보내기 위한 Props
-    const accomNum = accmData;
+    const accomNum = backEndData.accmData;
 
 
     /* 수정과 삭제를 위한 함수 시작 */
@@ -95,33 +100,44 @@ const AdminDetailAccm = () => {
     };
 
 
-    const fetchData = async () => {
-        try {
-            const res = await axios.post(`http://localhost:8090/api/admin/accm/show_accm_detail?a_m_no=${requestData.a_m_no}`);
-            //  res -> 서버에서 받아온 데이터
-            console.log("detail data success");
-            // res.data에서 얻은 데이터를 화면에 업데이트 하기 위해 data상태에 설정한다. data 상태를 업데이트 하면 화면이 새로 렌더링 된다.
-            console.log("확인 : ", res.data);
-            setAccmData(res.data.adminAccmDto);
-            console.log("accmData", accmData);
-            setImages(res.data.a_i_images);
-            // const imageUrls = images.a_i_images;
-            // setImages(imageUrls);
+
+    // console.log("accmData", accmData);
 
 
-        } catch (error) {
-            console.error("An error occurred:", error);
-        }
-    };
+
 
 
     useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.post(`http://localhost:8090/api/admin/accm/show_accm_detail?a_m_no=${requestData.a_m_no}`);
+                //  res -> 서버에서 받아온 데이터
+                console.log("detail data success");
+                // res.data에서 얻은 데이터를 화면에 업데이트 하기 위해 data상태에 설정한다. data 상태를 업데이트 하면 화면이 새로 렌더링 된다.
+
+                // setAccmData(res.data.adminAccmDto);
+                setBackEndData({
+                    accmData: res.data.adminAccmDto,
+                    accmImages: res.data.a_i_images
+                });
+                setDataLoaded(true);
+                // setImages(res.data.a_i_images);
+                // const imageUrls = images.a_i_images;
+                // setImages(imageUrls);
+
+
+            } catch (error) {
+                setDataLoaded(true);
+                console.error("An error occurred:", error);
+            }
+        };
+
         fetchData(); // 비동기 함수 호출
     }, [requestData]);
 
 
-    console.log("accmData", accmData);
-    console.log("image", images);
+    // console.log("accmData", accmData);
+    // console.log("image", images);
 
     // useEffect(() => {
     //     if (Array.isArray(images)) {
@@ -134,7 +150,30 @@ const AdminDetailAccm = () => {
     //     }
     // }, [images]);
 
+    if (!dataLoaded) {
+
+        return (
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                minHeight="100vh" // This ensures that the Box takes the full viewport height
+                sx={{
+                    backgroundColor: 'background.default', // Use theme background color
+                    color: 'text.primary', // Use theme text color
+                }}
+            >
+                {/* You can include a CircularProgress component to indicate loading status */}
+                <CircularProgress color="inherit" />
+                <Typography variant="h3" component="h1">
+                    Loading...
+                </Typography>
+            </Box>
+        );
+    }
+
     return (
+
 
         <Container component="main" sx={{ marginBottom: '3rem', marginTop: '3rem' }}>
             <Paper elevation={3} sx={{ padding: '2rem', display: 'flex', flexDirection: 'column' }}>
@@ -143,7 +182,7 @@ const AdminDetailAccm = () => {
                     <IconButton aria-label="수정" onClick={() => handleClickOpen('edit')}>
                         <EditIcon />
                     </IconButton>
-                    <ConfirmOrClose open={openEdit} close={() => close('edit')} confirmation={() => handleEditConfirmation(accmData.a_acc_name)} words="수정" />
+                    <ConfirmOrClose open={openEdit} close={() => close('edit')} confirmation={() => handleEditConfirmation(backEndData.accmData.a_m_no)} words="수정" />
 
                     <IconButton aria-label="삭제" onClick={() => handleClickOpen('delete')}>
                         <DeleteIcon />
@@ -154,7 +193,7 @@ const AdminDetailAccm = () => {
 
                 <Box sx={{ marginBottom: '1rem', marginTop: '1rem', backgroundColor: 'white', padding: '1rem' }}>
                     <Carousel>
-                        {images.map((imageUrl, index) => (
+                        {backEndData.accmImages.map((imageUrl, index) => (
                             <Paper key={index} sx={{ height: '180px', overflow: 'hidden' }}>
                                 <img src={imageUrl} alt={`Image ${index}`} style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
                             </Paper>
@@ -167,7 +206,7 @@ const AdminDetailAccm = () => {
 
 
                     <Grid container alignItems="center" sx={{ paddingLeft: '10px', paddingRight: '10px', fontSize: '30px' }}>
-                        {accmData.a_acc_name}
+                        {backEndData.accmData.a_acc_name}
                         <Grid item xs={10} sx={{ mt: '10px' }}>
                             <Divider variant="left" sx={{ width: '100%' }} />
                         </Grid>
@@ -177,19 +216,19 @@ const AdminDetailAccm = () => {
 
                 <Item sx={{ marginTop: '1rem' }}>
                     <Grid container xs={10} sx={{ alignSelf: 'flex-start', fontSize: '15px', marginTop: '8px', paddingLeft: '10px', paddingRight: '10px' }}>
-                        주소 : {accmData.a_acc_address}
+                        주소 : {backEndData.accmData.a_acc_address}
                     </Grid>
                     <Grid container xs={10} sx={{ alignSelf: 'flex-start', fontSize: '15px', marginTop: '8px', paddingLeft: '10px', paddingRight: '10px' }}>
-                        연락처 : {accmData.a_acc_phone}
+                        연락처 : {backEndData.accmData.a_acc_phone}
                     </Grid>
                     <Grid container xs={10} sx={{ alignSelf: 'flex-start', fontSize: '15px', marginTop: '8px', paddingLeft: '10px', paddingRight: '10px' }}>
-                        이메일 : {accmData.a_m_email}
+                        이메일 : {backEndData.accmData.a_m_email}
                     </Grid>
                     <Grid container xs={10} sx={{ alignSelf: 'flex-start', fontSize: '15px', marginTop: '8px', paddingLeft: '10px', paddingRight: '10px' }}>
-                        사업자 번호 : {accmData.a_acc_bn}
+                        사업자 번호 : {backEndData.accmData.a_acc_bn}
                     </Grid>
                     <Grid container xs={10} sx={{ alignSelf: 'flex-start', fontSize: '15px', marginTop: '8px', paddingLeft: '10px', paddingRight: '10px' }}>
-                        대표자 명 : {accmData.a_m_name}
+                        대표자 명 : {backEndData.accmData.a_m_name}
                     </Grid>
                     <Grid container alignItems="center" sx={{ paddingLeft: '10px', paddingRight: '10px' }}>
                         <Grid item xs={10} sx={{ mt: '10px' }}>
@@ -204,7 +243,7 @@ const AdminDetailAccm = () => {
                         업소 정보
                     </Grid>
                     <Grid container alignItems="center" sx={{ paddingLeft: '10px', paddingRight: '10px' }}>
-                        <span dangerouslySetInnerHTML={{ __html: accmData.a_acc_intro }}></span>
+                        <span dangerouslySetInnerHTML={{ __html: backEndData.accmData.a_acc_intro }}></span>
                     </Grid>
 
                     <Grid container alignItems="center" sx={{ paddingLeft: '10px', paddingRight: '10px' }}>
@@ -219,7 +258,7 @@ const AdminDetailAccm = () => {
                     <Grid container alignItems="center" sx={{ paddingLeft: '10px', paddingRight: '10px', fontSize: '20px' }}>
                         객실
                     </Grid>
-                    <AdminRoomList accomNum={accomNum} />
+                    <AdminRoomList accomNum={accomNum} requestData={requestData} />
                 </Item>
 
             </Paper>
--- src/pages/accommodation/admin/AdminRoomList.jsx
+++ src/pages/accommodation/admin/AdminRoomList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -9,13 +9,21 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import { useNavigate } from "react-router";
 import ConfirmOrClose from "../../../components/ConfirmOrClose";
+import axios from "axios";
 
-const AdminRoomList = ({ accomNum }) => {
+const AdminRoomList = ({ accomNum, requestData }) => {
 
     // 모달창 열기 //
     const [open, setOpen] = useState(false);
     const [name, setName] = useState('');
     const [items, setItems] = useState([]);
+
+    // 백엔드에서 온 데이터
+    const [backEndData, setBackEndData] = useState({
+        roomData: {},
+        roomImages: [],
+        roomImageNos: []
+    });
 
     const navigate = useNavigate();
 
@@ -28,6 +36,7 @@ const AdminRoomList = ({ accomNum }) => {
     };
 
     console.log("accomNum", accomNum.a_acc_no);
+    console.log("accomNum.a_m_no", accomNum.a_m_no);
 
     const handleRegistConfirmation = () => {
         navigate(`/admin/accommodation/registRoom/${accomNum.a_acc_no}`);
@@ -41,6 +50,34 @@ const AdminRoomList = ({ accomNum }) => {
             setOpen(false);
         }
     };
+
+
+    useEffect(() => {
+        fetchData(); // 비동기 함수 호출
+    }, [requestData]);
+
+    const fetchData = async () => {
+        try {
+            const res = await axios.post(`http://localhost:8090/api/admin/room/showRoomDetail?a_m_no=${accomNum.a_m_no}`);
+            //  res -> 서버에서 받아온 데이터
+            console.log("room data success");
+            // res.data에서 얻은 데이터를 화면에 업데이트 하기 위해 data상태에 설정한다. data 상태를 업데이트 하면 화면이 새로 렌더링 된다.
+            console.log("확인 : ", res.data);
+            setBackEndData({
+                roomData: res.data.adminRoomDto,
+                roomImages: res.data.r_i_images,
+                roomImageNos: res.data.r_i_nos
+            });
+            console.log("accmData", backEndData);
+            // setImages(res.data.a_i_images);
+            // const imageUrls = images.a_i_images;
+            // setImages(imageUrls);
+
+        } catch (error) {
+            console.error("에러 발생", error);
+        }
+    };
+
 
     return (
         <div>
