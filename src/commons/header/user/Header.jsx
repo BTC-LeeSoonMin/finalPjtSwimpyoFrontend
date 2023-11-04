1116072947997a6b9e8c490ad4e13db920eb1d0e
congyubin52 <congyubin52@gmail.com>
2023-11-04 19:28:51 +0900

    doing searchAccm - searchBar, searchAccmList

--- src/commons/header/user/Header.jsx
+++ src/commons/header/user/Header.jsx
@@ -1,57 +1,15 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
 import { setAccessToken } from '../../../commons/rtk/slice/SignInSlice';
 import { useSelector,useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import api from '../../../hooks/RefreshTokenAuto';
 import logo from '../../../assets/logo.png';
+import SearchBar from '../../../pages/accommodation/user/searchAccm/SearchBar';
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.black, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.black, 0.25),
-  },
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginRight: theme.spacing(7),
-    width: '100%',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
 
 const linkStyle = {
   color: 'black',
@@ -117,15 +75,7 @@ export default function Header() {
               <img style={{maxHeight: '80px'}} src={logo} alt="logo Image" />
             </Link>
           </Box>
-          <Search sx={{ flexGrow: 1 }}>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
+          <SearchBar />
           {!token && <Typography
             noWrap
             component="div"
---
+++ src/pages/accommodation/user/searchAccm/SearchBar.jsx
@@ -0,0 +1,96 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import api from '../../../../hooks/RefreshTokenAuto';
+import { useNavigate } from 'react-router-dom';
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.black, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.black, 0.25),
+    },
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginRight: theme.spacing(7),
+      width: '100%',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
+
+export default function SearchBar() {
+    const config = {
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8'
+        }
+      };
+
+    const navigate = useNavigate();
+
+    const searchEnter = (e) => {
+        if (e.key === 'Enter') {
+
+            const searchWord = e.target.value;
+            console.log('검색어', searchWord);
+
+            // let data = {};
+
+            // data = {
+            //     "searchWord": searchWord,
+            // }
+
+            // api.post("/api/user/searchAccm", JSON.stringify(data), config,)
+            //     .then((response) => {
+            //         console.log(response.data);
+            //         if (response.data !== null) {
+            //             navigate('/user/searchAccm');
+            //         }
+
+            //     });
+
+        }
+      };
+
+    return(
+
+        <Search sx={{ flexGrow: 1 }}>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="지역 또는 숙소명을 검색해보세요."
+              inputProps={{ 'aria-label': 'search' }}
+              onKeyDown={searchEnter}
+            />
+          </Search>
+
+    );
+}
